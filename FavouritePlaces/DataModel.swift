48b137f19c0e1c551e0c0f2d41e6e965cb63ac47
Samuel Lee <samuellee249@gmail.com>
2023-04-29 17:33:14 +1000

    Added data persistence to the DataModel

--- FavouritePlaces/DataModel.swift
+++ FavouritePlaces/DataModel.swift
@@ -7,13 +7,45 @@
 
 import Foundation
 
-struct Place: Hashable {
+struct Place: Hashable, Codable {
     var image: String
     var name: String
 }
 
-struct DataModel {
+func getFile() -> URL? {
+    let filename = "myplaces.json"
+    let fm = FileManager.default
+    guard let url = fm.urls(for: .documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask)
+        .first else {
+        return nil
+    }
+    return url.appendingPathComponent(filename)
+}
+
+struct DataModel: Codable {
     var locations:[Place]
+    init () {
+        locations = []
+        load()
+    }
+    
+    mutating func load() {
+        guard let url = getFile(),
+              let data = try? Data(contentsOf: url),
+              let datamodel = try? JSONDecoder().decode(DataModel.self, from: data) else {
+            self.locations = testLocations
+            return
+        }
+        self.locations = datamodel.locations
+    }
+    
+    func save() {
+        guard let url = getFile(),
+              let data = try? JSONEncoder().encode(self) else {
+            return
+        }
+        try? data.write(to: url)
+    }
 }
 
 var testLocations = [
