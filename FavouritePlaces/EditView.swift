d7157d56329e3d347d6aef8f06eb953c207626fc
Samuel Lee <samuellee249@gmail.com>
2023-04-29 17:23:40 +1000

    Added EditView

---
+++ FavouritePlaces/EditView.swift
@@ -0,0 +1,14 @@
+//
+//  EditView.swift
+//  FavouritePlaces
+//
+//  Created by Samuel Lee on 29/4/2023.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
