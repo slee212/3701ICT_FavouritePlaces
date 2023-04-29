67ad0b0bcbb103985ec910a1a6f88cc81a147afb
Samuel Lee <samuellee249@gmail.com>
2023-04-29 16:33:31 +1000

    Edited ContentView, created placeholder list of places

--- FavouritePlaces/ContentView.swift
+++ FavouritePlaces/ContentView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
+var places = [["doc.text.image.fill", "Brisbane"], ["doc.text.image.fill", "Gold Coast"], ["doc.text.image.fill", "Melbourne"]]
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List {
+                ForEach(places, id:\.self) {
+                    place in
+                    HStack {
+                        Image(systemName: "doc.text.image.fill")
+                        Text(place[1])
+                    }
+                }
+            }
         }
         .padding()
     }
