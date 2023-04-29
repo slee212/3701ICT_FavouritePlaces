475331f62fe5c93fc6231e67ca961352a6055f4a
Samuel Lee <samuellee249@gmail.com>
2023-04-29 16:22:43 +1000

    Initial Commit

---
+++ FavouritePlaces/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FavouritePlaces
+//
+//  Created by Samuel Lee on 29/4/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavouritePlaces/FavouritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePlacesApp.swift
+//  FavouritePlaces
+//
+//  Created by Samuel Lee on 29/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
