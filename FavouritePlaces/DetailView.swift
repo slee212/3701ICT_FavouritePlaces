9b8339782574449e49cbf4ed54c381261b3bf776
Samuel Lee <samuellee249@gmail.com>
2023-05-28 18:53:25 +1000

    Added search feature

--- FavouritePlaces/DetailView.swift
+++ FavouritePlaces/DetailView.swift
@@ -1,11 +1,3 @@
-//
-//  DetailView.swift
-//  FavouritePlaces
-//
-//  Created by Samuel Lee on 29/4/2023.
-//
-
-
 import SwiftUI
 import CoreData
 import CoreLocation
@@ -19,50 +11,61 @@ struct DetailView: View {
     @State var longitude: Double = 0.0
     @State var latitude: Double = 0.0
     @State var region: MKCoordinateRegion = MKCoordinateRegion()
+    @State private var locationName: String = ""
     var count: Int
     
     let decimalFormatter: NumberFormatter = {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         return formatter
-    } ()
-    
+    }()
+
     var body: some View {
-        EditView(item: $name)
-        HStack {
-            List {
-                HStack{
-                    if let imageUrl = URL(string: image) {
-                        ImageView(url: imageUrl)
+        VStack {
+            EditView(item: $name)
+            
+            HStack {
+                List {
+                    HStack {
+                        if let imageUrl = URL(string: image) {
+                            ImageView(url: imageUrl)
+                        }
+                    }
+                    
+                    TextField("URL:", text: $image)
+                    
+                    HStack {
+                        Text("Description:")
+                        TextField("Description", text: $desc)
+                    }
+                    
+                    TextField("Location Name:", text: $locationName, onEditingChanged: { _ in
+                        updateLocationFromName()
+                    })
+                    
+                    HStack {
+                        Map(coordinateRegion: $region)
+                            .frame(height: 300)
+                    }
+                    
+                    HStack {
+                        Text("Longitude:")
+                        TextField("Longitude", value: $longitude, formatter: decimalFormatter)
+                    }
+                    
+                    HStack {
+                        Text("Latitude:")
+                        TextField("Latitude", value: $latitude, formatter: decimalFormatter)
                     }
                 }
-                
-                TextField("URL:", text: $image)
-                HStack {
-                    Text("Description:")
-                    TextField("Description", text: $desc)
-                }
-                HStack{
-                    Map(coordinateRegion: $region)
-                        .frame(height: 300)
-                }
-                HStack {
-                    Text("Longitude:")
-                    TextField("Longitude", value: $longitude, formatter: decimalFormatter)
-                }
-                HStack {
-                    Text("Latitude:")
-                    TextField("Latitude", value: $latitude, formatter: decimalFormatter)
-                }
             }
-        }
-        .onChange(of: longitude) { newValue in
-            updateMapRegion()
-        }
-        .onChange(of: latitude) { newValue in
-            updateMapRegion()
-        }
-        .navigationTitle(name)
+            .onChange(of: longitude) { newValue in
+                updateMapRegion()
+            }
+            .onChange(of: latitude) { newValue in
+                updateMapRegion()
+            }
+            .navigationTitle(name)
             .navigationBarItems(trailing: EditButton())
             .onAppear {
                 image = place.locations[count].image
@@ -79,9 +82,29 @@ struct DetailView: View {
                 place.locations[count].longitude = longitude
                 place.locations[count].latitude = latitude
                 place.save()
-                
             }
+        }
     }
+    
+    private func updateLocationFromName() {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(locationName) { placemarks, error in
+            if let error = error {
+                print("Geocoding error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let placemark = placemarks?.first,
+                  let location = placemark.location else {
+                return
+            }
+            
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+            updateMapRegion()
+        }
+    }
+    
     private func updateMapRegion() {
         region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     }
