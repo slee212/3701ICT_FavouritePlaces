cac690cca6d7611e0c8751431f8dab07ac567998
Samuel Lee <samuellee249@gmail.com>
2023-05-28 19:46:24 +1000

    Added map details to location view. Updates map in real time when typing the location. Also updates coordinates when moving the map

--- FavouritePlaces/DetailView.swift
+++ FavouritePlaces/DetailView.swift
@@ -3,109 +3,40 @@ import CoreData
 import CoreLocation
 import MapKit
 
+import SwiftUI
+
 struct DetailView: View {
     @Binding var place: DataModel
-    @State var image: String = ""
-    @State var name: String = ""
-    @State var desc: String = ""
-    @State var longitude: Double = 0.0
-    @State var latitude: Double = 0.0
-    @State var region: MKCoordinateRegion = MKCoordinateRegion()
-    @State private var locationName: String = ""
     var count: Int
     
-    let decimalFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        return formatter
-    }()
-
     var body: some View {
         VStack {
-            EditView(item: $name)
+            EditView(item: $place.locations[count].name)
             
             HStack {
                 List {
                     HStack {
-                        if let imageUrl = URL(string: image) {
+                        if let imageUrl = URL(string: place.locations[count].image) {
                             ImageView(url: imageUrl)
                         }
                     }
                     
-                    TextField("URL:", text: $image)
+                    TextField("URL:", text: $place.locations[count].image)
                     
                     HStack {
                         Text("Description:")
-                        TextField("Description", text: $desc)
+                        TextField("Description", text: $place.locations[count].desc)
                     }
                     
-                    TextField("Location Name:", text: $locationName, onEditingChanged: { _ in
-                        updateLocationFromName()
-                    })
-                    
-                    HStack {
-                        Map(coordinateRegion: $region)
-                            .frame(height: 300)
-                    }
-                    
-                    HStack {
-                        Text("Longitude:")
-                        TextField("Longitude", value: $longitude, formatter: decimalFormatter)
-                    }
-                    
-                    HStack {
-                        Text("Latitude:")
-                        TextField("Latitude", value: $latitude, formatter: decimalFormatter)
+                    NavigationLink(destination: LocationView(place: $place, count: count)) {
+                        HStack {
+                            Text("Edit Location")
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.blue)
+                        }
                     }
                 }
             }
-            .onChange(of: longitude) { newValue in
-                updateMapRegion()
-            }
-            .onChange(of: latitude) { newValue in
-                updateMapRegion()
-            }
-            .navigationTitle(name)
-            .navigationBarItems(trailing: EditButton())
-            .onAppear {
-                image = place.locations[count].image
-                name = place.locations[count].name
-                desc = place.locations[count].desc
-                longitude = place.locations[count].longitude
-                latitude = place.locations[count].latitude
-                updateMapRegion()
-            }
-            .onDisappear() {
-                place.locations[count].image = image
-                place.locations[count].name = name
-                place.locations[count].desc = desc
-                place.locations[count].longitude = longitude
-                place.locations[count].latitude = latitude
-                place.save()
-            }
         }
-    }
-    
-    private func updateLocationFromName() {
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(locationName) { placemarks, error in
-            if let error = error {
-                print("Geocoding error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let placemark = placemarks?.first,
-                  let location = placemark.location else {
-                return
-            }
-            
-            latitude = location.coordinate.latitude
-            longitude = location.coordinate.longitude
-            updateMapRegion()
-        }
-    }
-    
-    private func updateMapRegion() {
-        region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     }
 }
--- FavouritePlaces/LocationView.swift
+++ FavouritePlaces/LocationView.swift
@@ -2,7 +2,170 @@
 //  LocationView.swift
 //  FavouritePlaces
 //
-//  Created by Christiaan on 28/5/2023.
+//  Created by Samuel Lee on 29/4/2023.
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
+import MapKit
+
+struct LocationView: View {
+    @Binding var place: DataModel
+    var count: Int
+    
+    @State private var editedName: String = ""
+    @State private var editedLongitude: Double = 0.0
+    @State private var editedLatitude: Double = 0.0
+    @State private var isEditing: Bool = false
+    @State private var displayedLocationName: String = ""
+    
+    let decimalFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
+    init(place: Binding<DataModel>, count: Int) {
+        _place = place
+        self.count = count
+        
+        let initialLocation = place.wrappedValue.locations[count]
+        _editedName = State(initialValue: initialLocation.name)
+        _editedLongitude = State(initialValue: initialLocation.longitude)
+        _editedLatitude = State(initialValue: initialLocation.latitude)
+        _displayedLocationName = State(initialValue: initialLocation.name)
+    }
+    
+    var body: some View {
+        VStack {
+            if isEditing {
+                TextField("Location Name", text: $editedName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+            } else {
+                Text(editedName)
+                    .font(.title)
+                    .padding()
+            }
+            
+            TextField("Longitude", value: $editedLongitude, formatter: decimalFormatter)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .disabled(!isEditing)
+            
+            TextField("Latitude", value: $editedLatitude, formatter: decimalFormatter)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .disabled(!isEditing)
+            
+            Text("Current Location: \(displayedLocationName)")
+                .font(.headline)
+                .padding()
+            
+            Map(coordinateRegion: coordinateRegion)
+                .frame(height: 300)
+                .onAppear {
+                    updateDisplayedLocationName()
+                }
+        }
+        .navigationBarItems(trailing: editButton)
+        .onChange(of: editedName) { _ in
+            updateCoordinates()
+            updateDisplayedLocationName()
+        }
+        .onChange(of: editedLongitude) { _ in
+            updateDisplayedLocationName()
+        }
+        .onChange(of: editedLatitude) { _ in
+            updateDisplayedLocationName()
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                backButton
+            }
+        }
+    }
+    
+    private var coordinateRegion: Binding<MKCoordinateRegion> {
+        Binding<MKCoordinateRegion>(
+            get: {
+                let center = CLLocationCoordinate2D(latitude: editedLatitude, longitude: editedLongitude)
+                let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                return MKCoordinateRegion(center: center, span: span)
+            },
+            set: { region in
+                editedLatitude = region.center.latitude
+                editedLongitude = region.center.longitude
+            }
+        )
+    }
+    
+    private var editButton: some View {
+        Button(action: {
+            isEditing.toggle()
+            
+            if !isEditing {
+                saveChanges()
+            }
+        }) {
+            Text(isEditing ? "Done" : "Edit")
+        }
+    }
+    
+    private var backButton: some View {
+        Button(action: {
+            saveChanges()
+        }) {
+            HStack {
+                Image(systemName: "chevron.left")
+                Text("Back")
+            }
+        }
+    }
+    
+    private func saveChanges() {
+        place.locations[count].name = editedName
+        place.locations[count].longitude = editedLongitude
+        place.locations[count].latitude = editedLatitude
+        place.save()
+    }
+    
+    private func updateCoordinates() {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(editedName) { placemarks, error in
+            if let placemark = placemarks?.first,
+               let location = placemark.location {
+                editedLatitude = location.coordinate.latitude
+                editedLongitude = location.coordinate.longitude
+            }
+        }
+    }
+    
+    private func updateDisplayedLocationName() {
+        getLocationName(for: editedLatitude, longitude: editedLongitude) { name in
+            displayedLocationName = name ?? ""
+        }
+    }
+    
+    private func getLocationName(for latitude: Double, longitude: Double, completion: @escaping (String?) -> Void) {
+        let location = CLLocation(latitude: latitude, longitude: longitude)
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            guard error == nil else {
+                completion(nil)
+                return
+            }
+            
+            if let placemark = placemarks?.first {
+                let name = placemark.name ?? ""
+                completion(name)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+}
